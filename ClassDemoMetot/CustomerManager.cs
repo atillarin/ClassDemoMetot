96e03c3dc47eaab60e95767d0132a2de9ea9283f
atillaarin.cokmez <atillaarin.cokmez@COKMEZATILLA>
2021-01-20 13:14:07 +0300

    .

--- ClassDemoMetot/CustomerManager.cs
+++ ClassDemoMetot/CustomerManager.cs
@@ -6,5 +6,22 @@ namespace ClassDemoMetot
 {
     class CustomerManager
     {
+        public void Add(Customer cus)
+        {
+            Console.WriteLine(cus.Name +" kullanıcısı eklenmiştir.");
+        }
+
+        public void Delete(Customer cus)
+        {
+            Console.WriteLine(cus.Name+ " kullanıcısı silinmiştir");
+        }
+
+        public void List(Customer[] cus)
+        {
+            foreach (var x in cus)
+            {
+                Console.WriteLine("Müşteri Adı :"+ x.Name +" Soyadı : "+ x.Surname);
+            }
+        }
     }
 }
