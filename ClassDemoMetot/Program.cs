2e34b934ca434f28f92c44749497ceca4c77ff4a
atillaarin.cokmez <atillaarin.cokmez@COKMEZATILLA>
2021-01-20 13:22:03 +0300

    .

--- ClassDemoMetot/Program.cs
+++ ClassDemoMetot/Program.cs
@@ -6,7 +6,18 @@ namespace ClassDemoMetot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("aaaaabbbccc");
+            Customer cus1 = new Customer() { Id =0,Name="Atilla",Surname="Çökmez" };
+            Customer cus2 = new Customer() { Id = 1, Name = "Rümeysa", Surname = "Doğan" };
+            Customer cus3 = new Customer() { Id = 2, Name = "Nehir", Surname = "Tuna" };
+            Customer[] customers = new Customer[] { cus1,cus2,cus3};
+
+            CustomerManager cusman = new CustomerManager();
+
+            cusman.Add(cus1);
+            cusman.Add(cus2);
+            cusman.List(customers);
+
+
         }
     }
 }
